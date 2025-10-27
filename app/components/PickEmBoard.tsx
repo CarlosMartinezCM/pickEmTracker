330b9a70204032a6bc7c1d13e43d27e8670978e3
CarlosMartinezCM <31548679+CarlosMartinezCM@users.noreply.github.com>
2025-10-27 07:55:59 -0700

    Update PickEmBoard.tsx

--- app/components/PickEmBoard.tsx
+++ app/components/PickEmBoard.tsx
@@ -53,7 +53,7 @@ type Result = { [gameIndex: number]: string };
 type LeaderboardPlayer = Player & { correct: number; wrong: number; rank: number };
 
 // Week 8 results
-const confirmedResults: (string | null)[] = [/* Week  Results */  "LAC", "MIA", "NYJ", "NE", "PHI", "BUF", "BAL", "HOU", "TB", "DEN", "IND"  /*Results*/];
+const confirmedResults: (string | null)[] = [/* Week  Results */  "LAC", "MIA", "NYJ", "NE", "PHI", "BUF", "BAL", "HOU", "TB", "DEN", "IND", "GB"  /*Results*/];
 
 //Week 8 players                                                 
 const initialPlayers: Player[] = [
